a1408479552467812c9caa4c878efc36684ba8a4
wesley <142819760+wesleysantos0@users.noreply.github.com>
2025-04-14 08:57:29 -0300

    Initial commit

---
+++ SRC/engine.js
@@ -0,0 +1,8 @@
+function posicionarMenuNoFinal() {
+    const menu = document.querySelector(".menuBotton");
+    const alturaJanela = window.innerHeight;
+    menu.style.top = (alturaJanela - menu) + "px";
+  }
+  
+  window.addEventListener("load", posicionarMenuNoFinal);
+  window.addEventListener("resize", posicionarMenuNoFinal);
