c11e5c5c232257f8ab16966d05ef27a160fd7d3b
wesley <142819760+wesleysantos0@users.noreply.github.com>
2025-04-21 17:17:09 -0300

    att001
    pagina de localização

--- SRC/engine.js
+++ SRC/engine.js
@@ -1,13 +1,24 @@
 function posicionarMenuNoFinal() {
-    const menu = document.querySelector(".menuBotton");
-    const alturaJanela = window.innerHeight;
-    menu.style.top = (alturaJanela - menu) + "px";
-  }
-  
-  window.addEventListener("load", posicionarMenuNoFinal);
-  window.addEventListener("resize", posicionarMenuNoFinal);
+  const menu = document.querySelector(".menuBotton");
+  const alturaJanela = window.innerHeight;
+  menu.style.top = (alturaJanela - menu) + "px";
+}
+
+window.addEventListener("load", posicionarMenuNoFinal);
+window.addEventListener("resize", posicionarMenuNoFinal);
 
 console.log(window.innerWidth);
 console.log(window.innerHeight);
 
+///  PAGINA DE LOCALIZACAO
+
+let pergunta = document.querySelector('.pergunta')
+
+let btnSim = document.querySelector('#sim');
+btnSim.addEventListener('click', dpNone());
+
+function dpNone(){
+  pergunta.style.cssText = "display:none;"
+  console.log('click');
   
+}
